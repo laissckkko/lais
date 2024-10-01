e67709cf0b77e7342f38c26748341ff951b70561
laissckkko <Lais.sckoteski.araujo@escola.pr.gov.br>
2024-10-01 11:51:56 -0300

    a

---
+++ main.js
@@ -0,0 +1,24 @@
+function calcularMedia() {
+    let valor1 = +prompt("Digite o primeiro valor:");
+    let valor2 = +prompt("Digite o segundo valor:");
+    let valor3 = +prompt("Digite o terceiro valor:");
+
+ 
+    let media = (valor1 + valor2 + valor3) / 3;
+
+ 
+    return media;
+}
+
+
+let resultado = calcularMedia();
+
+document.write('A média dos tres valores é: '${resultado})
+
+let nome = ['lais', 'arthur', 'adam', 'maria eduarda', 'andrew',' luis', 'luiz',' joao', 'vinicius', 'danilo', 'ana', 'giovana',' nicolas', 'monise', 'caio', 'caio henry', 'arthur h', 'felipe', 'acacio', 'lucas']
+let i = 0,
+while ( i<10){
+    console.log(i);
+console.log(nome[i]);
+i=i+1;
+}
